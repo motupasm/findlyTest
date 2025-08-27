10db2cbf8546d6a0c11de0d437a92e10c4f02edf
unknown <motupams@gmail.com>
2025-08-27 23:56:11 +0200

    remove whitenoise

--- findlyprj/settings.py
+++ findlyprj/settings.py
@@ -63,7 +63,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "findlyprj.urls"
