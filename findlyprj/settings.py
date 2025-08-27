f4db924f38c0c38e32063caa121bd3e02346843f
unknown <motupams@gmail.com>
2025-08-27 23:53:38 +0200

    remove unused imports

--- findlyprj/settings.py
+++ findlyprj/settings.py
@@ -89,8 +89,6 @@ WSGI_APPLICATION = "findlyprj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import dj_database_url
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
